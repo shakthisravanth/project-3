eee0f9ba69629adc63cc36b978d48aa88224c53a
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2025-12-23 12:37:11 +0530

    Create Main.java

---
+++ LearningInheritance/app/Main.java
@@ -0,0 +1,23 @@
+// package declaration
+
+// import Person and Student classes
+
+// Main class
+
+// main method
+
+// create Person object
+
+// assign name to person
+
+// display person name
+
+// create Student object
+
+// assign name to student
+
+// assign roll number to student
+
+// display student name
+
+// display student roll number
